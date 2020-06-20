123522b1ab8c5e6b3589d1d7fde6da25359c01bd
Yukikazari <haya_nanakusa793@yahoo.co.jp>
2020-06-20 09:30:11 +0900

    SetWindowのDataContextとりあえず確定。必要になったらまた増やす

--- TimeTable/TimeTable/Processing.cs
+++ TimeTable/TimeTable/Processing.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace TimeTable
 {
@@ -18,7 +22,24 @@ namespace TimeTable
 
     public class TimetableSetting
     {
+        public string period { get; set; }
 
+        public int day_st { get; set; }
+
+        public int day_en { get; set; }
+
+        public string timer_music { get; set; }
+
+        public bool display_mon { get; set; }
+
+        public bool display_tue { get; set; }
+
+        public bool display_wed { get; set; }
+        public bool display_thu { get; set; }
+
+        public bool display_fri { get; set; }
+
+        public bool display_sat { get; set; }
     }
 
     public class Lecture
@@ -58,5 +79,167 @@ namespace TimeTable
         public string name { get; set; }
 
         public string time { get; set; }
+    }
+
+    public class SetViewModel : INotifyPropertyChanged
+    {
+
+        private string _period; // 時限
+        private int _day_st; // いつから
+        private int _day_en; // いつまで。IDで入れとく予定。方法はわからん
+        private string _timer_music; // タイマー音
+        private bool _display_mon; // げつ
+        private bool _display_tue; // かー
+        private bool _display_wed; // すい
+        private bool _display_thu; // もく
+        private bool _display_fri; // きん
+        private bool _display_sat; // どー
+
+        public List<Obj_Combobox> _obj_tf;
+        public List<Obj_Combobox> _obj_day_st;
+        public List<Obj_Combobox> _obj_day_en;
+
+        public string period
+        {
+            get { return this._period; }
+            set
+            {
+                this._period = value;
+                this.OnPropertyChanged(nameof(period));
+            }
+        }
+
+        public int day_st
+        {
+            get { return this._day_st; }
+            set
+            {
+                this._day_st = value;
+                this.OnPropertyChanged(nameof(day_st));
+            }
+        }
+
+        public int day_en
+        {
+            get { return this._day_en; }
+            set
+            {
+                this._day_en = value;
+                this.OnPropertyChanged(nameof(day_en));
+            }
+        }
+
+        public string timer_music
+        {
+            get { return this._timer_music; }
+            set
+            {
+                this._timer_music = value;
+                this.OnPropertyChanged(nameof(timer_music));
+            }
+        }
+
+        public bool display_mon
+        {
+            get { return this._display_mon; }
+            set
+            {
+                this._display_mon = value;
+                this.OnPropertyChanged(nameof(display_mon));
+            }
+        }
+
+        public bool display_tue
+        {
+            get { return this._display_tue; }
+            set
+            {
+                this._display_tue = value;
+                this.OnPropertyChanged(nameof(display_tue));
+            }
+        }
+
+        public bool display_wed
+        {
+            get { return this._display_wed; }
+            set
+            {
+                this._display_wed = value;
+                this.OnPropertyChanged(nameof(display_wed));
+            }
+        }
+
+        public bool display_thu
+        {
+            get { return this._display_thu; }
+            set
+            {
+                this._display_thu = value;
+                this.OnPropertyChanged(nameof(display_thu));
+            }
+        }
+
+        public bool display_fri
+        {
+            get { return this._display_fri; }
+            set
+            {
+                this._display_fri = value;
+                this.OnPropertyChanged(nameof(display_fri));
+            }
+        }
+
+        public bool display_sat
+        {
+            get { return this._display_sat; }
+            set
+            {
+                this._display_sat = value;
+                this.OnPropertyChanged(nameof(display_sat));
+            }
+        }
+
+        public List<Obj_Combobox> obj_tf
+        {
+            get { return this._obj_tf; }
+            set
+            {
+                this._obj_tf = value;
+                this.OnPropertyChanged(nameof(obj_tf));
+            }
+        }
+
+        public List<Obj_Combobox> obj_day_st
+        {
+            get { return this._obj_day_st; }
+            set
+            {
+                this._obj_day_st = value;
+                this.OnPropertyChanged(nameof(obj_day_st));
+            }
+        }
+
+        public List<Obj_Combobox> obj_day_en
+        {
+            get { return this._obj_day_en; }
+            set
+            {
+                this._obj_day_en = value;
+                this.OnPropertyChanged(nameof(obj_day_en));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged = null;
+        protected void OnPropertyChanged(string info)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+    }
+
+    public class Obj_Combobox
+    {
+        public int id { get; set; }
+        public string name { get; set; }
     }
 }
--- TimeTable/TimeTable/SetWindow.xaml.cs
+++ TimeTable/TimeTable/SetWindow.xaml.cs
@@ -22,6 +22,71 @@ namespace TimeTable
         public SetWindow()
         {
             InitializeComponent();
+
+            _viewModel = new SetViewModel();
+            this.DataContext = _viewModel;
+
+            SetWindowConf();
+        }
+
+        private SetViewModel _viewModel;
+
+        void SetWindowConf()
+        {
+            // TimetableSetting set = MainWindow.data.setting;
+
+            TimetableSetting set = new TimetableSetting();
+
+            _viewModel.period = set.period;
+
+            _viewModel.day_st = set.day_st;
+            _viewModel.day_en = set.day_en;
+
+            _viewModel.display_mon = set.display_mon;
+            _viewModel.display_tue = set.display_tue;
+            _viewModel.display_wed = set.display_wed;
+            _viewModel.display_thu = set.display_thu;
+            _viewModel.display_fri = set.display_fri;
+            _viewModel.display_sat = set.display_sat;
+
+            List<Obj_Combobox> tmp = new List<Obj_Combobox>();
+
+            tmp.Add(CreateObj(1, "表示"));
+            tmp.Add(CreateObj(0, "非表示"));
+
+            _viewModel.obj_tf = tmp;
+
+            tmp = new List<Obj_Combobox>();
+
+            tmp.Add(CreateObj(0, "今日"));
+            tmp.Add(CreateObj(1, "1日前"));
+            tmp.Add(CreateObj(2, "2日前"));
+            tmp.Add(CreateObj(3, "3日前"));
+
+            _viewModel.obj_day_st = tmp;
+
+            tmp = new List<Obj_Combobox>();
+
+            tmp.Add(CreateObj(0, "今日"));
+            tmp.Add(CreateObj(1, "1日後"));
+            tmp.Add(CreateObj(2, "2日後"));
+            tmp.Add(CreateObj(3, "3日後"));
+
+            _viewModel.obj_day_en = tmp;
+
+            
+
+            this.DataContext = _viewModel;
+        }
+
+        Obj_Combobox CreateObj(int id_in, string name_in)
+        {
+            var o = new Obj_Combobox()
+            {
+                id = id_in, name = name_in
+            };
+
+            return o;
         }
     }
 }
